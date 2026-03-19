d77be5f5ab4f1ca01d7d9867c0a7412ccffd51e0
QuerCoder <vladfank@gmail.com>
2026-03-19 16:31:39 +0300

    Add plugin name/description localization for Russian locale

--- includes/class-ucg-plugin.php
+++ includes/class-ucg-plugin.php
@@ -11,6 +11,7 @@ if (!class_exists('UCG_Plugin')) {
 
         public function run() {
             $this->maybe_upgrade();
+            add_filter('all_plugins', [$this, 'localize_plugin_meta']);
             add_filter('cron_schedules', array('UCG_Generator', 'register_cron_schedule'));
 
             $this->admin = new UCG_Admin();
@@ -20,6 +21,19 @@ if (!class_exists('UCG_Plugin')) {
             $this->generator->hooks();
 
             UCG_Generator::ensure_worker_scheduled();
+        }
+
+        public function localize_plugin_meta(array $plugins): array {
+            $file = 'unicontent-ai-generator/unicontent-ai-generator.php';
+            if (!isset($plugins[$file])) {
+                return $plugins;
+            }
+            $locale = get_locale();
+            if (strpos($locale, 'ru_') === 0 || $locale === 'ru') {
+                $plugins[$file]['Name']        = 'UniContent AI — Генератор контента';
+                $plugins[$file]['Description'] = 'Генерирует описания товаров, SEO-метатеги и тексты для WordPress и WooCommerce. Массовая обработка каталога.';
+            }
+            return $plugins;
         }
 
         protected function maybe_upgrade() {
